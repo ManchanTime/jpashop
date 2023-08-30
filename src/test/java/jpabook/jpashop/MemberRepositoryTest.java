5b9a440d5bf7b747b14919830b35c93f281d559a
Lee <lych0918@gmail.com>
2023-08-30 13:41:30 +0900

    회원 리포지토리, 회원 서비스 기능 생성

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -12,22 +12,11 @@ import static org.assertj.core.api.Assertions.*;
 @SpringBootTest
 class MemberRepositoryTest {
 
-    @Autowired
-    MemberRepository memberRepository;
-
     @Test
     @Transactional //테스트 끝나면 DB 롤백해버림!!
     //@Rollback(value = false) 테스트 끝나도 DB 유지함!!
     void save(){
         //given
-        Member member = new Member();
-        member.setUsername("memberA");
-        //when
-        Long savedId = memberRepository.save(member);
-        //then
-        Member findMember = memberRepository.find(savedId);
-        assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        assertThat(findMember).isEqualTo(member);
+
     }
 }
